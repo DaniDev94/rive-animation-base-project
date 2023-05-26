4b044c1703c5a18d44999bff252a08dc01ec6c53
Daniel López <daniel.lp1094@gmail.com>
2023-05-26 17:37:16 +0200

    fix: change paths in js files

--- src/pages/basic_concepts/index.js
+++ src/pages/basic_concepts/index.js
@@ -20,7 +20,7 @@ function riveObjects() {
     const $popup = document.getElementById("popup");
 
     const loading = new rive.Rive({
-        src: "/animations/loading.riv",
+        src: "animations/loading.riv",
         canvas: document.getElementById("loading"),
         autoplay: true,
         layout: new rive.Layout({ fit: "cover" }),
